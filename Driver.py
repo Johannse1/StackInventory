2f61227a49b33c2827a6c8a0227a17ea4c32eb91
Evan <johannse@hartwick.edu>
2020-03-24 16:57:36 -0400

    update

--- Driver.py
+++ Driver.py
@@ -6,7 +6,6 @@ from Stack import Stack
 
 total_inventory = 0
 total_profit = 0
-
 My_Inventory = Stack()
 
 menu_choice = int(input(""" Please type the # of the action you want to perform.
@@ -22,12 +21,13 @@ while menu_choice < 1 or menu_choice > 5:
     data = 0
     if menu_choice == 1:
         while data <= 1:
-            data = int(input("please ener the amount of inv. being added: "))
-            Stack.push(data)
+            data = int(input("please enter the amount of inv. being added: "))
+            total_inventory = total_inventory + data
+            My_Inventory.push(data)
 
             # this is where i am stuck, after runnung this code and entering '1', the code exits instead of going into the next loop
 
-    elif menu_choice == 2:
+    # elif menu_choice == 2:
 
 
 
